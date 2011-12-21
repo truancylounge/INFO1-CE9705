1d9cb7f30ecbf33ee8e7b62a96a6a83c54f64713
Lalith <truancylounge@gmail.com>
2011-12-21 04:42:56 -0500

    Landscape Activity.

---
+++ LandActivity/src/edu/lal/LandActivityActivity.java
@@ -0,0 +1,13 @@
+package edu.lal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LandActivityActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
